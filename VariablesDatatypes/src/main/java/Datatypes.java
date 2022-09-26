32b1cda7083749c2a67f6f200accf2119614a6c3
Pavlov83 <resolversoft@gmail.com>
2022-09-26 22:27:17 +0300

    Datatypes integer

---
+++ VariablesDatatypes/src/main/java/Datatypes.java
@@ -0,0 +1,11 @@
+public class Datatypes {
+
+    public static void main(String[] args) {
+
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println("Integer minimum value " + minValue);
+        System.out.println("Integer maximum value  " + maxValue);
+
+    }
+}
